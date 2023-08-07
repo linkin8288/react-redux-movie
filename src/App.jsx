51bdfba67d605778a0e6c3c586cdacafed659004
charlie <linkin8288@gmail.com>
2023-08-07 22:12:49 +0200

    Add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import Login from './pages/Login';
 import Netflix from './pages/Netflix';
 import Signup from './pages/Signup';
+import Player from './pages/Player';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path='/login' element={<Login />} />
-        <Route exact path='/signup' element={<Signup />} />
-        <Route exact path='/' element={<Netflix />} />
+      <Route exact path="/login" element={<Login />} />
+        <Route exact path="/signup" element={<Signup />} />
+        <Route exact path="/player" element={<Player />} />
+        <Route exact path="/" element={<Netflix />} />
       </Routes>
     </BrowserRouter>
   )
